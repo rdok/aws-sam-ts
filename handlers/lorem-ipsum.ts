f9cf679c6b9bec2eb0c84050bbc870db03a880c6
Rizart Dokollari <r.dokollari@gmail.com>
2021-12-18 15:34:58 +0000

    HTTP response

---
+++ handlers/lorem-ipsum.ts
@@ -0,0 +1,15 @@
+import { dolorSit } from "../lib/ioc";
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+} from "aws-lambda/trigger/api-gateway-proxy";
+
+export async function handle(
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> {
+  const lorem = await dolorSit.find();
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ lorem, eventBody: event.body }),
+  };
+}
