b379241361ed0349212f8d2e934c6117ed0bb9d9
Rizart Dokollari <r.dokollari@gmail.com>
2021-12-18 15:39:21 +0000

    Import configuration

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  testEnvironment: 'node',
+  preset: 'ts-jest',
+  testMatch: ['**/?(*.)+(spec).ts'],
+  testPathIgnorePatterns: ['<rootDir>/cicd-infrastructure', '<rootDir>/.aws-sam', '<rootDir>/node_modules'],
+  transformIgnorePatterns: ['^.+\\.js$', 'node_modules'],
+  setupFilesAfterEnv: ['<rootDir>/jest/setupFilesAfterEnv.ts'],
+  setupFiles: ['<rootDir>/jest/setupFiles.ts'],
+  modulePaths: ['<rootDir>/jest/factories'],
+  transform: {
+    '.(ts|tsx)': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      compiler: 'ttypescript',
+    },
+  },
+};
