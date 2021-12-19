2524bc4bb4c794c1a8e43f791a786cce3d070a4e
Rizart Dokollari <r.dokollari@gmail.com>
2021-12-19 21:15:08 +0000

    Use ts-jest preset

--- cicd-authorisation/jest.config.js
+++ cicd-authorisation/jest.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
+  testEnvironment: "node",
+  preset: "ts-jest",
 };
