e79f8254784956699e14e810d8e2f8c20590b2d0
Rizart Dokollari <r.dokollari@gmail.com>
2021-12-19 21:16:26 +0000

    Replace assertions with template

--- cicd-authorisation/test/outputs.test.ts
+++ cicd-authorisation/test/outputs.test.ts
@@ -1,17 +1,14 @@
-import { expect as expectCDK, haveOutput } from "@aws-cdk/assert";
 import { App } from "@aws-cdk/core";
 import { CICDAuthorisationStack } from "../lib/cicd-authorisation-stack";
 import { Config } from "../lib/config";
+import { Template } from "@aws-cdk/assertions";
 
 const app = new App();
 const config = new Config();
 const stack = new CICDAuthorisationStack(app, "MyTestStack", config);
+const template = Template.fromStack(stack);
 
 test("Create output for CI/CD bucket name", () => {
-  expectCDK(stack).to(
-    haveOutput({
-      outputName: "CICDBucketName",
-      outputValue: { Ref: "CICDBucket2E34B30B" },
-    })
-  );
+  const actual = template.findOutputs("CICDBucketName");
+  expect(actual.CICDBucketName.Value.Ref).toMatch(/CICDBucket.+/);
 });
