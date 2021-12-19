2a69218963b86ea7005c47d80a338aa6194f4392
Rizart Dokollari <r.dokollari@gmail.com>
2021-12-19 21:17:57 +0000

    Remove stale user

--- cicd-authorisation/lib/types.ts
+++ cicd-authorisation/lib/types.ts
@@ -1,8 +1,6 @@
-import { User } from '@aws-cdk/aws-iam';
-import { Config } from './config';
+import { Config } from "./config";
 
 export type PolicyProps = {
-  user: User;
   config: Config;
   stackRegex: string;
 };
