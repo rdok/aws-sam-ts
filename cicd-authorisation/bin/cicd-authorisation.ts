82295d49d845c3a968d3142818ea7cb14ec1ccee
Rizart Dokollari <r.dokollari@gmail.com>
2021-12-20 04:20:09 +0000

    Decrease cdk import scope

--- cicd-authorisation/bin/cicd-authorisation.ts
+++ cicd-authorisation/bin/cicd-authorisation.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import "source-map-support/register";
-import * as cdk from "@aws-cdk/core";
+import { App } from "@aws-cdk/core";
 import { CICDAuthorisationStack } from "../lib/cicd-authorisation-stack";
 import { Config } from "../lib/config";
 
-const app = new cdk.App();
+const app = new App();
 const config = new Config();
 
 const stackName = `${config.org}-${config.cicdEnvironment}-${config.name}`;
